b2947c3d9297aabb634d27214eb8d99be8af2aca
curefate <66023115+curefate@users.noreply.github.com>
2026-03-08 16:48:00 +0100

    + show panel * fix bug at F3 grow platform

--- Assets/Workplace/Scripts/Panel/PassCodePanel.cs
+++ Assets/Workplace/Scripts/Panel/PassCodePanel.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,8 +9,31 @@ public class PassCodePanel : MonoBehaviour
 
     private int correctRequire;
     private int validPressTimes;
-    public UnityEvent OnPasscodeCorrect;
-    public UnityEvent OnPasscodeWrong;
+
+    [SerializeField] private UnityEvent OnPasscodeCorrect;
+    [SerializeField] private UnityEvent OnPasscodeWrong;
+    [SerializeField] private Transform hideTransform;
+    [SerializeField] private Transform showTransform;
+    private Coroutine _transitionCoroutine;
+    private const float TransitionDuration = 1f;
+
+    public void ShowPanel()
+    {
+        if (_transitionCoroutine != null)
+        {
+            StopCoroutine(_transitionCoroutine);
+        }
+        _transitionCoroutine = StartCoroutine(AlignToTransforms(showTransform, TransitionDuration));
+    }
+
+    public void HidePanel()
+    {
+        if (_transitionCoroutine != null)
+        {
+            StopCoroutine(_transitionCoroutine);
+        }
+        _transitionCoroutine = StartCoroutine(AlignToTransforms(hideTransform, TransitionDuration));
+    }
 
     private void Start()
     {
@@ -26,6 +50,8 @@ public class PassCodePanel : MonoBehaviour
         {
             button.SetPanel(this);
         }
+
+        _transitionCoroutine = StartCoroutine(AlignToTransforms(hideTransform, 0.01f));
     }
 
     public void PressButton(bool isCorrect)
@@ -69,5 +95,25 @@ public class PassCodePanel : MonoBehaviour
         {
             button.Reset();
         }
+    }
+
+    private IEnumerator AlignToTransforms(Transform target, float duration)
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = transform.position;
+        Quaternion initialRotation = transform.rotation;
+        Vector3 initialScale = transform.localScale;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            transform.SetPositionAndRotation(Vector3.Lerp(initialPosition, target.position, t), Quaternion.Slerp(initialRotation, target.rotation, t));
+            transform.localScale = Vector3.Lerp(initialScale, target.localScale, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.SetPositionAndRotation(target.position, target.rotation);
+        transform.localScale = target.localScale;
     }
 }
