0923df636ae8b1561244600f1e219f2b0b173961
curefate <66023115+curefate@users.noreply.github.com>
2026-04-03 19:19:57 +0200

    modify volume

--- Assets/Workplace/Scripts/Charlie/PushToMoveButton.cs
+++ Assets/Workplace/Scripts/Charlie/PushToMoveButton.cs
@@ -28,6 +28,10 @@ public class PushToMoveButton : MonoBehaviour
         {
             nodeMover.MoveTowardTo(target);
         }
+        else
+        {
+            onNoTarget.Invoke();
+        }
     }
 
     void OnDrawGizmosSelected()
